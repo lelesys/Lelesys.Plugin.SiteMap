e985e861babd54a55bbe640486bed57d460c54ed
Gauri Shirodkar <gauri.shirodkar@lelesys.com>
2013-07-08 16:55:21 +0530

    [TASK] Updated documentation of class file.

--- Classes/Lelesys/Plugin/SiteMap/TypoScript/SiteMapFormat.php
+++ Classes/Lelesys/Plugin/SiteMap/TypoScript/SiteMapFormat.php
@@ -3,7 +3,7 @@
 namespace Lelesys\Plugin\SiteMap\TypoScript;
 
 /*                                                                        *
- * This script belongs to the TYPO3 Flow package "Lelesys.Plugin.SiteMap".*
+ * This script belongs to the package "Lelesys.Plugin.SiteMap".           *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
