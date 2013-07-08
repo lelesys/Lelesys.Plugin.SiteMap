cd7c3348062bf5a465e4845ae09273e6f59bbacb
Sidhesh Sherlekar <sidhesh.sherlekar@lelesys.com>
2013-07-08 16:23:22 +0530

    [TASK] Added open source license for class file.

--- Classes/Lelesys/Plugin/SiteMap/TypoScript/SiteMapFormat.php
+++ Classes/Lelesys/Plugin/SiteMap/TypoScript/SiteMapFormat.php
@@ -4,6 +4,10 @@ namespace Lelesys\Plugin\SiteMap\TypoScript;
 
 /*                                                                        *
  * This script belongs to the TYPO3 Flow package "Lelesys.Plugin.SiteMap".*
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
  *                                                                        */
 
 use TYPO3\Flow\Annotations as Flow;
